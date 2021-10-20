3f2da6c27ebd3350158bf4dc98b1ab50bac33232
Hayden (reoccurcat) <reoccurcat@gmail.com>
2021-10-19 20:47:42 -0500

    fixed

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k --quiet
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions --quiet
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --quiet
-wget --quiet "https://github.com/reoccurcat/dotfiles/raw/main/.zshrc" -p "~/"
-wget --quiet "https://github.com/reoccurcat/dotfiles/raw/main/.p10k.zsh" -p "~/"
-echo "All done, run 'zsh'"
+rm -r .zshrc
+wget --quiet -O $HOME/.zshrc "https://github.com/reoccurcat/dotfiles/raw/main/.zshrc"
+wget --quiet -O $HOME/.p10k.zsh "https://github.com/reoccurcat/dotfiles/raw/main/.p10k.zsh"
+echo "All done, restart your terminal session"
